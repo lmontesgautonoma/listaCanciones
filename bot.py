a21fac98e157bd5bcfe2cda61947a3ad7531c3bc
lmontes2 <lmontes@idiger.gov.co>
2021-02-09 13:33:10 -0500

    Creación de proyecto de la lista de canciones BOT

---
+++ bot.py
@@ -0,0 +1,43 @@
+#########################################################
+from config import bot
+import config
+from time import sleep
+import re
+#######################START##################################
+@bot.message_handler(commands=['start'])
+def on_command_start(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    bot.send_message(
+        message.chat.id,
+        "\U0001F3B8 Hola Mi nombre es Music \U0001F3BC,\U0001F601 \U0001F601 Bienvenidos!  \U0001F601 \U0001F601 ",
+        parse_mode="Markdown")@bot.message_handler(commands=['help'])
+##########################HELP###############################
+@bot.message_handler(commands=['help'])
+def on_command_help(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    response = (
+                "Estos son los comandos y órdenes disponibles:\n"
+                "\n"
+                "*/start* - Inicia la interacción con el bot\n"
+                "*/help* - Muestra este mensaje de ayuda\n"
+                )
+
+    bot.send_message(
+        message.chat.id,
+        response,
+    parse_mode="Markdown") 
+##################Siempre al final#####################    
+@bot.message_handler(func=lambda message: True)
+def on_fallback(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+
+    bot.reply_to(
+        message,
+        "\U0001F648 Ups, no entendí lo que me dijiste.")
+#########################################################
+if __name__ == '__main__':
+    bot.polling(timeout=20)
+#########################################################
